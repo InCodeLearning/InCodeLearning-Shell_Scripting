9d59a7e6ff057c527a9f303ba7826d5dc00d8180
Zexi Jesse Zhuang <zhzexi@gmail.com>
2016-09-01 11:55:13 -0700

    git fetch all script

---
+++ linux/fetchall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Usage: fetchall.sh branch ...
+
+set -xe
+git fetch --all
+for branch in "$@"; do
+  git checkout $branch || exit 1
+  git pull --rebase || exit 1
+done
+
+if [ $branch != "jesse" ]; then
+  git checkout jesse
+fi

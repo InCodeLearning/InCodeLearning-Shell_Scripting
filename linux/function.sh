a24bc494269c652fa4ae2f94b357f79a31ac51dd
Jesse Zhuang <zhzexi@gmail.com>
2021-02-23 18:49:44 -0800

    add back up file function

--- linux/function.sh
+++ linux/function.sh
@@ -18,3 +18,14 @@ hello_all() {
 }
 
 hello_all Mary Lamb Cat
+
+function backup_file() {
+  if [ -f "$1" ]
+  then
+    local BACKUP_FILE="/tmp/$(basename ${1}).$(date +%F).$$" # $$ current PID to avoid override each time
+    echo "Backing up $1 to ${BACKUP_FILE}"
+    cp $1 $BACKUP_FILE # exit status of function will be exit status of this cp command
+  else
+    return 1
+  fi
+}
